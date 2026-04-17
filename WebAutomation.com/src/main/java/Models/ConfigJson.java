090d4fd66448dfefb54a0cd44cb9ee4ed334ec2b
avinash1403 <avinash.bit.2011@gmail.com>
2026-04-17 14:54:23 +0530

    Updated file

---
+++ WebAutomation.com/src/main/java/Models/ConfigJson.java
@@ -0,0 +1,17 @@
+package Models;
+
+public class ConfigJson {
+
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+}
+

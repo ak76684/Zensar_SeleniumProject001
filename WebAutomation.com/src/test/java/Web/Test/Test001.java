9dee56971eb15ff2ab0fe83486193c53ba0f74ed
avinash1403 <avinash.bit.2011@gmail.com>
2026-04-23 23:49:00 +0530

    Small changes

--- WebAutomation.com/src/test/java/Web/Test/Test001.java
+++ WebAutomation.com/src/test/java/Web/Test/Test001.java
@@ -1,15 +1,46 @@
 package Web.Test;
 
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import Base.BaseTest;
 import Base.PropertyReaderFile;
 
-public class Test001 {
+public class Test001 extends BaseTest {
 	
-	@Test
+	//@Test
 	public void Test001() {
-	System.out.println("Printing first test");
+		int count=0;
+	for(int i =0 ; i<100;i++) {
+		String st = String.valueOf(i);
+		for(int j =0 ; j<st.length(); j++) {
+			if(st.charAt(j)=='9') {
+				count=count+1;
+			}
+		}
+	}
+	System.out.println(count);
 	
 	}
+	
+	@Test
+	public void test002() {
+		String str ="h$LLO wORLD!";
+		String nestr="";
+		for(int i =0; i<str.length(); i++) {
+			char ch = str.charAt(i);
+			String st = String.valueOf(ch);
+			if(Character.isLowerCase(ch)) {
+				
+				nestr=nestr+st.toUpperCase();
+			}else {
+				nestr=nestr+st.toLowerCase();
+			}
+		}
+		System.out.println(nestr);
+		
+	}
+	
 	
 
 }
